8dd620ef0a30880da6691c70cd9e54eb01ed2b72
Fusa-F <cist.b218.f.fukushi@gmail.com>
2020-11-14 02:25:02 +0900

    [add]Q1.3の解答を追加

--- with_python/Q13.py
+++ with_python/Q13.py
@@ -1,13 +1,34 @@
+# coding: utf-8
 import MeCab
 
 
 def print_words_and_results(raw_text: str = 'すもももももももものうち'):
-    pass
+    meCab = MeCab.Tagger('-Ochasen')
+    print(meCab.parse(raw_text))
 
 
 def print_morpheme(raw_text: str = 'すもももももももものうち'):
-    pass
+    select_condition = ['名詞']
+    meCab = MeCab.Tagger('-Ochasen')
+    # ノードごとに分ける
+    node = meCab.parseToNode(raw_text)
+    terms = []
+    while node:
+        # 単語
+        term = node.surface
+        # 品詞
+        pos = node.feature.split(',')[0]
+
+        if pos in select_condition:
+            terms.append(term)
+
+        node = node.next
+
+    print(' '.join(terms))
 
 
 if __name__ == '__main__':
-    pass
+    print('- Q1.3.1')
+    print_words_and_results()
+    print('- Q1.3.2')
+    print_morpheme()
